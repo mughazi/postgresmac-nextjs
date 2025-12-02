34075205e7e353b9558c31525ccfc1935ca10d8c
mughazi <fikrigha@gmail.com>
2025-12-01 22:41:09 -0500

    Update basepath

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath:
-    process.env.NODE_ENV === "development" ? "" : "/postgresmac-website",
+  basePath: "",
   images: {
     unoptimized: true,
   },
